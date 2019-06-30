6061ca9c0d51f43fc6ad5735fab45b8ecd42f826
Danil <makoveyfedorich@gmail.com>
2019-06-30 11:10:52 +0300

    Переработана функция отрисовки прямоугольника вокруг многоугольника.

--- Geometry.py
+++ Geometry.py
@@ -95,8 +95,64 @@ class Rectangle(object):
             plt.plot([rect[i][0], rect[i + 1][0]], [rect[i][1], rect[i + 1][1]], color='r')
 
 
-fig, ax = plt.subplots(1)
-rect = Rectangle(Dot(1, 1), Dot(1, 5), Dot(4, 5), Dot(4, 1))
-print(rect.build_field_coverage(0.1))
+class Polygon(object):
+    def __init__(self, dots):
+        self.dots = dots # Массив точек
+        self.dots.append(self.dots[0])
+
+    # Возвращает многоугольник в виде списка его точек
+    def get_as_array(self):
+        return self.dots
+
+    # Возвращает многоугольник в виде списка его линий - граней
+    def get_as_lines(self):
+        lines = []
+        for i in range(0, len(self.dots) - 1):
+            lines.append(Line(self.dots[i], self.dots[i + 1]))
+        return lines
+
+    # Возвращает площадь многоугольника
+    def get_area(self):
+        s1 = 0
+        s2 = 0
+        for i in range(0, len(self.dots) - 1):
+            s1 += self.dots[i].x * self.dots[i + 1].y
+            s2 += self.dots[i].y * self.dots[i + 1].x
+        return (s1 - s2) / 2
+
+    # Рисует двухмерный многоугольник - все его грани и точки
+    def plot_poly(self, color):
+        for i in range(len(self.dots) - 1):
+            plt.plot([self.dots[i].x, self.dots[i + 1].x], [self.dots[i].y, self.dots[i + 1].y],
+                     color=color)
+            plt.scatter(self.dots[i].x, self.dots[i].y, color=color)
+            plt.annotate('('+str(self.dots[i].x)+', '+ str(self.dots[i].y)+')',
+                         xy=(self.dots[i].x, self.dots[i].y-15),
+                         color = 'r', style = 'oblique', weight = 'bold')
+
+
+def draw_rect_around(polygon):
+    max_x = polygon[0].x
+    max_y = polygon[0].y
+    min_x = polygon[0].x
+    min_y = polygon[0].y
+
+    for dot in polygon:
+        if dot.x > max_x:
+            max_x = dot.x
+        if dot.x < min_x:
+            min_x = dot.x
+        if dot.y > max_y:
+            max_y = dot.y
+        if dot.y < min_y:
+            min_y = dot.y
+
+    # Возвращает двухмерный прямоугольник
+    return Rectangle(Dot(min_x, min_y), Dot(max_x, min_y), Dot(max_x, max_y), Dot(min_x, max_y))
+
+
+poly = Polygon([Dot(1, 4), Dot(5, 3), Dot(6, 8)])
+poly.plot_poly('b')
+rect = draw_rect_around([Dot(1, 4), Dot(5, 3), Dot(6, 8)])
 rect.plot_rect()
 plt.show()
