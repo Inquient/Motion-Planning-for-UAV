98b5e2f93b1d027eb0a0fb5609099c72f574d653
Danil <makoveyfedorich@gmail.com>
2019-09-28 20:39:41 +0300

    Функционал, отвечающий за отрисовку перенесён в отдельный модуль. Геометрические объекты упорядочены в единую структуру, вызывающий их код переписан с учётом их новых особенностей.

--- FieldCoverage.py
+++ FieldCoverage.py
@@ -4,21 +4,14 @@ from RRTLib import *
 import matplotlib.lines
 import matplotlib.patches as patches
 import scipy.interpolate
+from Geometry import Dot,Line,Rectangle,Polygon,draw_rect_around,lines_cross
 import time
-
 
 # Функция позволяет задать дробный рендж
 def frange(x, y, jump):
     while x < y:
         yield x
         x += jump
-
-
-# Функция находин расстояние между двумя точками
-def distance(x1, y1, x2, y2):
-    return np.sqrt(np.power(x2 - x1, 2) +
-                   np.power(y2 - y1, 2))
-
 
 # Функция удаляет повторяющиеся элементы из списка
 def remove_duplicates(l):
@@ -27,190 +20,6 @@ def remove_duplicates(l):
         if i not in n:
             n.append(i)
     return n
-
-def get_area(dots):
-    s1 = 0
-    s2 = 0
-    for i in range(0, len(dots) - 1):
-        s1 += dots[i][0] * dots[i + 1][1]
-        s2 += dots[i][1] * dots[i + 1][0]
-    return (s2 - s1) / 2
-
-class Dot(object):
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-
-# Класс прямой - линии
-class Line(object):
-    def __init__(self, start_dot, end_dot):
-        self.start_dot = start_dot
-        self.end_dot = end_dot
-
-    # Возвращает длинну прямой
-    def get_line_length(self):
-        return np.sqrt(np.power(self.end_dot[0] - self.start_dot[0], 2) +
-                       np.power(self.end_dot[1] - self.start_dot[1], 2))
-
-    # Возвращает прямую в виде списка из начальной и конечной точки
-    def get_as_array(self):
-        return [self.start_dot, self.end_dot]
-
-
-# Класс двухмерного прямоугольника
-class Rectangle(object):
-    def __init__(self, left_bottom, right_bottom, right_top, left_top):
-        self.left_bottom = left_bottom
-        self.right_bottom = right_bottom
-        self.right_top = right_top
-        self.left_top = left_top
-
-    # Возвращает квадрат в виде четырёх точек
-    def get_as_array(self):
-        return [self.left_bottom, self.right_bottom, self.right_top, self.left_top, self.left_bottom]
-
-    # Находит короткую сторону в прямоугольнике и возвращает её в виде двух точек
-    def get_short_side(self):
-        a = Line(self.left_bottom, self.right_bottom)
-        b = Line(self.left_bottom, self.left_top)
-
-        a_len, b_len = a.get_line_length(), b.get_line_length()
-
-        if a_len < b_len:
-            return a.get_as_array(), b.get_as_array()
-        else:
-            return b.get_as_array(), a.get_as_array()
-
-    # Строит в прямоугольнике предполагаемые линии облёта
-    def build_field_coverage(self, camera_angle):
-        short_side, long_side = self.get_short_side()
-        camera_angle = camera_angle - camera_angle * 0.20
-        start_dots = []
-        end_dots = []
-        if short_side[0][0] != short_side[1][0]:
-            for i in frange(short_side[0][0] + camera_angle, short_side[0][1], camera_angle):
-                start_dots.append((i, short_side[0][1]))
-                end_dots.append((i, long_side[0][1]))
-
-        if short_side[0][1] != short_side[1][1]:
-            for i in frange(short_side[0][1] + camera_angle, short_side[1][1], camera_angle):
-                start_dots.append((short_side[0][0], i))
-                end_dots.append((long_side[1][0], i))
-
-        for dot in start_dots:
-            dot = (round(dot[0], 2), round(dot[1], 2))
-            # plt.scatter(dot[0], dot[1], color = 'k')
-        for dot in end_dots:
-            dot = (round(dot[0], 2), round(dot[1], 2))
-            # plt.scatter(dot[0], dot[1], color = 'k')
-
-        result = []
-        for i in range(0, len(start_dots)):
-            result.append(Line(start_dots[i], end_dots[i]))
-        # Возвращает список линий облёта
-        return result
-
-    # Рисует двухмерный прямоугольник
-    def plot_rect(self):
-        rect = self.get_as_array()
-        for i in range(len(rect) - 1):
-            plt.plot([rect[i][0], rect[i + 1][0]], [rect[i][1], rect[i + 1][1]], color='r')
-
-
-# Класс двухмерного многоугольника
-class Polygon(object):
-    def __init__(self, dots):
-        self.dots = dots
-        self.dots.append(self.dots[0])
-
-    # Возвращает многоугольник в виде списка его точек
-    def get_as_array(self):
-        return self.dots
-
-    # Возвращает многоугольник в виде списка его линий - граней
-    def get_as_lines(self):
-        lines = []
-        for i in range(0, len(self.dots) - 1):
-            lines.append(Line(self.dots[i], self.dots[i + 1]))
-        return lines
-
-    # Возвращает площадь многоугольника
-    def get_area(self):
-        s1 = 0
-        s2 = 0
-        for i in range(0, len(dots) - 1):
-            s1 += dots[i][0] * dots[i + 1][1]
-            s2 += dots[i][1] * dots[i + 1][0]
-        return (s1 - s2) / 2
-
-    # Рисует двухмерный многоугольник - все его грани и точки
-    def plot_poly(self, color):
-        for i in range(len(self.dots) - 1):
-            plt.plot([self.dots[i][0], self.dots[i + 1][0]], [self.dots[i][1], self.dots[i + 1][1]],
-                     color=color)
-            plt.scatter(self.dots[i][0], self.dots[i][1], color=color)
-            plt.annotate('('+str(self.dots[i][0])+', '+ str(self.dots[i][1])+')',
-                         xy=(self.dots[i][0], self.dots[i][1]-15),
-                         color = 'r', style = 'oblique', weight = 'bold')
-
-    def plot_poly_in_3d(self, ax, z_cords):
-        for i in range(len(self.dots) - 1):
-            ax.plot([self.dots[i][0], self.dots[i + 1][0]], [self.dots[i][1], self.dots[i + 1][1]], z_cords,
-                     color='b')
-
-
-# Функция находит точку пересечения двух прямых
-# На вход принимает координаты начальной и конечной точки линии облёта, затем - грани многоугольника
-# def lines_cross(x1, y1, x2, y2, a1, b1, a2, b2):
-def lines_cross(f_line, p_edge):
-    A1 = f_line.start_dot[1] - f_line.end_dot[1]
-    B1 = f_line.end_dot[0] - f_line.start_dot[0]
-    C1 = f_line.start_dot[0] * f_line.end_dot[1] - f_line.end_dot[0] * f_line.start_dot[1]
-    A2 = p_edge.start_dot[1] - p_edge.end_dot[1]
-    B2 = p_edge.end_dot[0] - p_edge.start_dot[0]
-    C2 = p_edge.start_dot[0] * p_edge.end_dot[1] - p_edge.end_dot[0] * p_edge.start_dot[1]
-
-    x, y = 0, 0
-
-    if B1 * A2 - B2 * A1 and A1:
-        y = (C2 * A1 - C1 * A2) / (B1 * A2 - B2 * A1)
-        x = (-C1 - B1 * y) / A1
-    elif B1 * A2 - B2 * A1 and A2:
-        y = (C2 * A1 - C1 * A2) / (B1 * A2 - B2 * A1)
-        x = (-C2 - B2 * y) / A2
-
-    d = distance(p_edge.start_dot[0], p_edge.start_dot[1], p_edge.end_dot[0], p_edge.end_dot[1])
-    d1 = distance(p_edge.start_dot[0], p_edge.start_dot[1], x, y)
-    d2 = distance(p_edge.end_dot[0], p_edge.end_dot[1], x, y)
-
-    # Если найденная точка лежит на грани многоугольника, то возвращаем её, иначе - 0
-    if abs((d1 + d2) - d) <= 0.01:
-        return (x, y)
-    else:
-        return 0
-
-
-# Функция находит прямоугольник, описывающий данный многоугольник
-def draw_rect_around(polygon_dots):
-    max_x = polygon_dots[0][0]
-    max_y = polygon_dots[0][1]
-    min_x = polygon_dots[0][0]
-    min_y = polygon_dots[0][1]
-
-    for dot in polygon_dots:
-        if dot[0] > max_x:
-            max_x = dot[0]
-        if dot[0] < min_x:
-            min_x = dot[0]
-        if dot[1] > max_y:
-            max_y = dot[1]
-        if dot[1] < min_y:
-            min_y = dot[1]
-
-    # Возвращает двухмерный прямоугольник
-    return Rectangle((min_x, min_y), (max_x, min_y), (max_x, max_y), (min_x, max_y))
-
 
 # Функция для интерполяции пути, подходит как для 2 так и для 3 измерений
 def interpolate_path(arr):
@@ -235,22 +44,23 @@ def smooth_path(arr, rank):
 # Задаём двухмерное пространство
 fig, ax = plt.subplots(1)
 
-# Ещё один вариант многоугольника
-# dots = [(1, 4), (4, 2), (4, 4), (16, 4), (24, 7), (24, 19), (17, 19), (8, 23), (6,13), (1,7)]
-
 # Задаём многоугольник в виде множества точек и строим вокруг него прямоугольник
-dots = [(150, 37.5), (300, 75), (300, 150), (337.5, 112.5), (412.5, 112.5), (512, 150), (512, 262.5),
-        (412.5, 262.5), (412.5, 412.5), (300, 487.5), (262.5, 412.5), (262.5, 300), (187.5, 300),
-        (150, 375), (75, 300), (112.5, 225), (37.5, 150), ]
-rect = draw_rect_around(dots)
-# rect.plot_rect()
+p = Polygon([Dot(150, 37.5), Dot(300, 75), Dot(300, 150),
+                Dot(337.5, 112.5), Dot(412.5, 112.5),
+                Dot(512, 150), Dot(512, 262.5),
+                Dot(412.5, 262.5), Dot(412.5, 412.5),
+                Dot(300, 487.5), Dot(262.5, 412.5),
+                Dot(262.5, 300), Dot(187.5, 300),
+                Dot(150, 375), Dot(75, 300),
+                Dot(112.5, 225), Dot(37.5, 150), ])
+# Рисуем прямоугольник вокруг заданного многоугольника
+rect = draw_rect_around(p)
 
 # Задаём поле зрения камеры и строим линии облёта
 camera_angle = 31.0  # В данном случае он равен 15.5, значить ширина одной полосы - 15.5 метров
 flight_lines = rect.build_field_coverage(camera_angle)
 
 # Строим многоугольник из точек и рисуем его
-p = Polygon(dots)
 poly_area = p.get_area()
 print(poly_area)  # Получаем площадь многоугольника
 p.plot_poly('b')
@@ -387,7 +197,7 @@ for i in frange (0, 550, 20):
     plt.axvline(x=i, linewidth=0.5, color='k')
     plt.axhline(y=i, linewidth=0.5, color='k')
     for j in frange (0, 550, 20):
-        plt.scatter(i,j,color = 'k', s = 2)
+        plt.scatter( i, j, color = 'k', s = 2)
 
 # a_star_dots = route
 # a_star_dots = []
--- Geometry.py
+++ Geometry.py
@@ -1,23 +1,32 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
+# Данный модуль содержит методы для работы с геометрическими фигурами:
+# точками, прямыми, прямо- и многоугольниками
+
+
 # Функция позволяет задать дробный рендж
 def frange(x, y, jump):
     while x < y:
         yield x
         x += jump
 
+# Функция находин расстояние между двумя точками
+def distance(x1, y1, x2, y2):
+    return np.sqrt(np.power(x2 - x1, 2) +
+                   np.power(y2 - y1, 2))
 
+# Класс Точка, состоит из двух координат, целых или дробных
 class Dot(object):
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
+    # Возвращает точку в виде списка её координат
     def to_array(self):
         return [self.x, self.y]
 
-
-# Прямая состоит из двух объектов класса точка
+# Класс Прямая, состоит из двух объектов класса точка - наальной и конечной
 class Line(object):
     def __init__(self, start_dot, end_dot):
         self.start_dot = start_dot
@@ -28,11 +37,11 @@ class Line(object):
         return np.sqrt(np.power(self.end_dot.x - self.start_dot.x, 2) +
                        np.power(self.end_dot.y - self.start_dot.y, 2))
 
-    # Возвращает прямую в виде списка из начальной и конечной точек
+    # Возвращает прямую в виде списка из списков с координатими начальной и конечной точек
     def get_as_array(self):
         return [self.start_dot.to_array(), self.end_dot.to_array()]
 
-
+# Класс Прямоугольник, состоит из четырёх точек
 class Rectangle(object):
     def __init__(self, left_bottom_dot, right_bottom_dot, right_top_dot, left_top_dot):
         self.left_bottom_dot = left_bottom_dot
@@ -40,12 +49,12 @@ class Rectangle(object):
         self.right_top_dot = right_top_dot
         self.left_top_dot = left_top_dot
 
-    # Возвращает список всех вершин прямокгольника в виде координат их точек
+    # Возвращает список всех вершин прямоугольника в списка из списков с координатими их точек
     def get_as_array(self):
         return [self.left_bottom_dot.to_array(), self.right_bottom_dot.to_array(),
                 self.right_top_dot.to_array(), self.left_top_dot.to_array(), self.left_bottom_dot.to_array()]
 
-    # Находит короткую сторону в прямоугольнике и возвращает её вмести с длинной
+    # Находит короткую сторону в прямоугольнике и возвращает её вмести с длинной стороной
     def get_short_side(self):
         a = Line(self.left_bottom_dot, self.right_bottom_dot)
         b = Line(self.left_bottom_dot, self.left_top_dot)
@@ -67,19 +76,19 @@ class Rectangle(object):
 
         if short_side.start_dot.x != short_side.end_dot.x:
             for i in frange(short_side.start_dot.x  + camera_angle, short_side.end_dot.x, camera_angle):
-                start_dots.append((i, short_side.start_dot.y))
-                end_dots.append((i, long_side.start_dot.y))
+                start_dots.append(Dot(i, short_side.start_dot.y))
+                end_dots.append(Dot(i, long_side.start_dot.y))
 
         if short_side.start_dot.y != short_side.end_dot.y:
             for i in frange(short_side.start_dot.y + camera_angle, short_side.end_dot.y, camera_angle):
-                start_dots.append((short_side.start_dot.x, i))
-                end_dots.append((long_side.end_dot.x, i))
+                start_dots.append(Dot(short_side.start_dot.x, i))
+                end_dots.append(Dot(long_side.end_dot.x, i))
 
         for dot in start_dots:
-            dot = (round(dot[0], 2), round(dot[1], 2))
+            dot = (round(dot.x, 2), round(dot.y, 2))
             # plt.scatter(dot[0], dot[1], color = 'k')
         for dot in end_dots:
-            dot = (round(dot[0], 2), round(dot[1], 2))
+            dot = (round(dot.x, 2), round(dot.y, 2))
             # plt.scatter(dot[0], dot[1], color = 'k')
 
         result = []
@@ -89,12 +98,13 @@ class Rectangle(object):
         return result
 
     # Рисует двухмерный прямоугольник
+    # Для кооректной работы этого метода требуется инициализировать в matplotlib двухмернй пространство
     def plot_rect(self):
         rect = self.get_as_array()
         for i in range(len(rect) - 1):
             plt.plot([rect[i][0], rect[i + 1][0]], [rect[i][1], rect[i + 1][1]], color='r')
 
-
+# Класс Многоугольник, продставлен в виде закольцованного массива точек
 class Polygon(object):
     def __init__(self, dots):
         self.dots = dots # Массив точек
@@ -121,6 +131,7 @@ class Polygon(object):
         return (s1 - s2) / 2
 
     # Рисует двухмерный многоугольник - все его грани и точки
+    # Для кооректной работы этого метода требуется инициализировать в matplotlib двухмернй пространство
     def plot_poly(self, color):
         for i in range(len(self.dots) - 1):
             plt.plot([self.dots[i].x, self.dots[i + 1].x], [self.dots[i].y, self.dots[i + 1].y],
@@ -130,14 +141,15 @@ class Polygon(object):
                          xy=(self.dots[i].x, self.dots[i].y-15),
                          color = 'r', style = 'oblique', weight = 'bold')
 
-
+# Функция рисует прямоугольник вокруг многоугольника
+# Принимает объект типа Многоугольник
 def draw_rect_around(polygon):
-    max_x = polygon[0].x
-    max_y = polygon[0].y
-    min_x = polygon[0].x
-    min_y = polygon[0].y
+    max_x = polygon.dots[0].x
+    max_y = polygon.dots[0].y
+    min_x = polygon.dots[0].x
+    min_y = polygon.dots[0].y
 
-    for dot in polygon:
+    for dot in polygon.dots:
         if dot.x > max_x:
             max_x = dot.x
         if dot.x < min_x:
@@ -150,9 +162,37 @@ def draw_rect_around(polygon):
     # Возвращает двухмерный прямоугольник
     return Rectangle(Dot(min_x, min_y), Dot(max_x, min_y), Dot(max_x, max_y), Dot(min_x, max_y))
 
+# Функция находит точку пересечения двух прямых
+def lines_cross(f_line, p_edge):
+    A1 = f_line.start_dot.y - f_line.end_dot.y
+    B1 = f_line.end_dot.x - f_line.start_dot.x
+    C1 = f_line.start_dot.x * f_line.end_dot.y - f_line.end_dot.x * f_line.start_dot.y
+    A2 = p_edge.start_dot.y - p_edge.end_dot.y
+    B2 = p_edge.end_dot.x - p_edge.start_dot.x
+    C2 = p_edge.start_dot.x * p_edge.end_dot.y - p_edge.end_dot.x * p_edge.start_dot.y
 
-poly = Polygon([Dot(1, 4), Dot(5, 3), Dot(6, 8)])
-poly.plot_poly('b')
-rect = draw_rect_around([Dot(1, 4), Dot(5, 3), Dot(6, 8)])
-rect.plot_rect()
-plt.show()
+    x, y = 0, 0
+
+    if B1 * A2 - B2 * A1 and A1:
+        y = (C2 * A1 - C1 * A2) / (B1 * A2 - B2 * A1)
+        x = (-C1 - B1 * y) / A1
+    elif B1 * A2 - B2 * A1 and A2:
+        y = (C2 * A1 - C1 * A2) / (B1 * A2 - B2 * A1)
+        x = (-C2 - B2 * y) / A2
+
+    d = p_edge.get_line_length()
+    d1 = distance(p_edge.start_dot.x, p_edge.start_dot.y, x, y)
+    d2 = distance(p_edge.end_dot.x, p_edge.end_dot.y, x, y)
+
+    # Если найденная точка лежит на грани многоугольника, то возвращаем её, иначе - 0
+    if abs((d1 + d2) - d) <= 0.01:
+        return (x, y)
+    else:
+        return 0
+
+
+# poly = Polygon([Dot(1, 4), Dot(5, 3), Dot(6, 8)])
+# # poly.plot_poly('b')
+# # rect = draw_rect_around(poly)
+# # rect.plot_rect()
+# # plt.show()
